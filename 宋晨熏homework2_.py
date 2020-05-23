ca13d78a1cb0c5f6fc8fa7f61268ef6c4e90bf36
Aaron10310777 <65432901+Aaron10310777@users.noreply.github.com>
2020-05-23 10:38:40 +0800

    Add files via upload

---
+++ 宋晨熏homework2_.py
@@ -0,0 +1,21 @@
+
+
+
+
+score = input("score ")
+
+score=int(score)
+
+
+if (math >= 90 and eng >=90):
+    print("gift")
+elif(math <60 and eng <60):
+    print("hit")
+elif(math <60 or eng <60)
+    print("keep going")
+else:
+    print("pass")
+
+
+
+
